970f099369d28c2d24662c001a15794c3eab328d
htlee55 <htlee55@naver.com>
2026-04-22 10:49:56 +0900

    Refactor main function and include headers

--- chapter4/BankSimulator/BankSimulator.cpp
+++ chapter4/BankSimulator/BankSimulator.cpp
@@ -1,11 +1,13 @@
 // 04장-BankSimulator.cpp: 은행 시뮬레이션 프로그램
-#include <time.h>
+#include <ctime>	// 시간 관련 라이브러리
+#include <cstdlib>	// 난수 생성 라이브러리
 #include "BankSimulator.h"
-void main()
+int main()
 {
-	srand( (unsigned int)time(NULL) );
-	BankSimulator	sim;
-	sim.readSimulationParameters( );
-	sim.run();		
-	sim.printStat();
+	srand((unsigned int)time(NULL));		// 난수 생성 시드 설정
+	BankSimulator sim;						// 시뮬레이터 객체 생성
+	sim.readSimulationParameters();			// 시뮬레이션 파라미터 설정
+	sim.run();								// 시뮬레이션 시작
+	sim.printStat();						// 시뮬레이션 결과 출력
+	return 0;
 }
