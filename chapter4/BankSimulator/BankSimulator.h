b60d5e816c6d3ecebc7d04ec5738d373ce535981
htlee55 <htlee55@naver.com>
2026-04-22 10:50:46 +0900

    Refactor BankSimulator.h for code consistency

--- chapter4/BankSimulator/BankSimulator.h
+++ chapter4/BankSimulator/BankSimulator.h
@@ -1,68 +1,69 @@
 #pragma once
 #include "CustomerQueue.h"
-
+#include <cstdio>
+ 
 class BankSimulator
 {
-	int	nSimulation;		
-	double	probArrival;	
-	int	tMaxService;		
-	int	totalWaitTime;		
-	int	nCustomers;			
-	int	nServedCustomers;	
-	CustomerQueue	que;	
+	int	nSimulation;			// 시뮬레이션 할 최대 시간
+	double	probArrival;		// 단위 시간에 도착하는 고객 수
+	int	tMaxService;			// 한 고객에 대한 최대 서비스 시간
+	int	totalWaitTime;			// 전체 대기 시간
+	int	nCustomers;				// 현재까지 도착한 고객 수
+	int	nServedCustomers;		// 서비스 받은 고객 수
+	CustomerQueue	que;		// 대기 고객 큐
 
-	double Random()		{ return rand()/(double)RAND_MAX; }
-	bool IsNewCustomer()	{ return Random() > probArrival; }
-	int RandServiceTime()	{ return (int)(tMaxService*Random())+1; }
+	double Random() { return rand() / (double)RAND_MAX; }
+	bool IsNewCustomer() { return Random() > probArrival; }
+	int RandServiceTime() { return (int)(tMaxService * Random()) + 1; }
 
-	void InsertCustomer( int arrivalTime ) {
+	void InsertCustomer(int arrivalTime) {
 		Customer a(++nCustomers, arrivalTime, RandServiceTime());
 		printf("  고객 %d 방문 (서비스 시간:%d분)\n", a.id, a.tService);
-		que.enqueue( a );
+		que.enqueue(a);
 	}
 
 public:
-	BankSimulator(): nCustomers(0),totalWaitTime(0),nServedCustomers(0){}
+	BankSimulator() : nCustomers(0), totalWaitTime(0), nServedCustomers(0) {}
 
-	void readSimulationParameters( ) {
+	void readSimulationParameters() {
 		printf("시뮬레이션 할 최대 시간 (예:10) = ");
-		scanf_s("%d", &nSimulation);
-		scanf_s("단위시간에 도착하는 고객 수 (예:0.5) = ");
-		scanf_s("%lf", &probArrival);
-		scanf_s("한 고객에 대한 최대 서비스 시간 (예:5) = ");
-		scanf_s("%d", &tMaxService);
+		scanf_s("%d", &nSimulation, sizeof(nSimulation));
+		printf("단위시간에 도착하는 고객 수 (예:0.5) = ");
+		scanf_s("%lf", &probArrival, sizeof(probArrival));
+		printf("한 고객에 대한 최대 서비스 시간 (예:5) = ");
+		scanf_s("%d", &tMaxService, sizeof(tMaxService));
 		printf("====================================================\n");
 	}
 
 	void run() {
-		int	clock = 0;		
+		int	clock = 0;
 		int	serviceTime = -1;
-		while(clock < nSimulation){
+		while (clock < nSimulation) {
 			clock++;
 			printf("현재시각=%d\n", clock);
 
-			if ( IsNewCustomer())	
+			if (IsNewCustomer())
 				InsertCustomer(clock);
-			if ( serviceTime>0 ) serviceTime--;	
+			if (serviceTime > 0) serviceTime--;
 			else {
-				if( que.isEmpty() ) continue;	
-				Customer a = que.dequeue();		
-				nServedCustomers++;	 			
-				totalWaitTime += clock-a.tArrival;
+				if (que.isEmpty()) continue;
+				Customer a = que.dequeue();
+				nServedCustomers++;
+				totalWaitTime += clock - a.tArrival;
 				printf("  고객 %d 서비스 시작 (대기시간:%d분)\n",
-					a.id, clock-a.tArrival);
-				serviceTime = a.tService-1;
+					a.id, clock - a.tArrival);
+				serviceTime = a.tService - 1;
 			}
-		} 
+		}
 	}
 
 	void printStat() {
 		printf("=================================================\n");
-		printf("  서비스 받은 고객수      = %d\n",	nServedCustomers);
+		printf("  서비스 받은 고객수      = %d\n", nServedCustomers);
 		printf("  전체 대기 시간          = %d분\n", totalWaitTime);
 		printf("  서비스고객 평균대기시간 = %-5.2f분\n",
-			(double)totalWaitTime/nServedCustomers);
-		printf("  현재 대기 고객 수       = %d\n", nCustomers-nServedCustomers);
+			(double)totalWaitTime / nServedCustomers);
+		printf("  현재 대기 고객 수       = %d\n", nCustomers - nServedCustomers);
 
 	}
 };
