8dc36863bd3636910c7468e99a3474179fe1a0b7
htlee55 <htlee55@naver.com>
2026-04-29 12:19:56 +0900

    Add CircularCustomerQueue class for customer management

---
+++ midterm/student_meeting_priority/CircularCustomerQueue.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "Customer.h"
+#define MAX_QUEUE_SIZE 100
+#include <cstdio>
+#include <cstdlib>
+
+inline void error(const char* message) {
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
+
+class CircularCustomerQueue
+{
+protected:
+	Customer data[MAX_QUEUE_SIZE];
+	int front;
+	int rear;
+public:
+	CircularCustomerQueue() : front(0), rear(0) {
+	}
+	~CircularCustomerQueue() {}
+	bool isEmpty() { return front == rear; }
+	bool isFull() { return (rear + 1) % MAX_QUEUE_SIZE == front; }
+	void enqueue(const Customer& customer) {
+		if (isFull()) return; // Queue is full
+		data[rear] = customer;
+		rear = (rear + 1) % MAX_QUEUE_SIZE;
+	}
+	Customer& dequeue() {
+		if (isEmpty()) error("Queue is empty"); // Queue is empty
+		else {
+			Customer& customer = data[front];
+			front = (front + 1) % MAX_QUEUE_SIZE;
+			return customer;
+		}
+	}
+	Customer& peek() {
+		if (isEmpty()) error("Queue is empty"); // Queue is empty
+		else {
+			return data[front];
+		}
+	}
+	int size() const {
+		if (rear >= front) {
+			return rear - front;
+		}
+		else {
+			return MAX_QUEUE_SIZE - (front - rear);
+		}
+	}
+};
