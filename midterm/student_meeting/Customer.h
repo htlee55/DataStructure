bacf4647b42b9d0d11ffe42e41d51b029c1d6d28
htlee55 <htlee55@naver.com>
2026-04-29 15:12:28 +0900

    Implement Customer struct with constructors and methods
    Added a Customer struct with constructors and member functions.

--- midterm/student_meeting/Customer.h
+++ midterm/student_meeting/Customer.h
@@ -1,1 +1,20 @@
+#pragma once
+#include <cstring>
+struct Customer
+{
+	int id;
+	char gender;
+	char name[20];
+	char priority;
+public:
+	Customer() : id(0), gender('m'), name{ '\0' }, priority('n') {}
+	Customer(int id, char gen = 'm', const char* name = "", char pri = 'n') : id(id), gender(gen), priority(pri) {
+		strcpy_s(this->name, sizeof(this->name), name);	// C-style string copy, buffer overflow prevented
+	}
+	~Customer() { }
+	const char* getName() const { return name; }
+	char getGender() const { return gender; }
+	char getPriority() const { return priority; }
+	int getId() const { return id; }
+};
 
