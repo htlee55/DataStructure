9e2983a76a7ae91feb2444cc28fb3f78dbdb02b6
htlee55 <htlee55@naver.com>
2026-05-26 18:59:46 +0900

    Refactor BinaryTree class for consistency and clarity

--- chapter8/BinaryTree/BinaryTree.h
+++ chapter8/BinaryTree/BinaryTree.h
@@ -4,32 +4,32 @@
 
 class BinaryTree
 {
-	BinaryNode*	root;
+	BinaryNode* root;
 public:
 
-	BinaryTree(): root(NULL) { }
-	~BinaryTree()	{ }
+	BinaryTree() : root(NULL) {}
+	~BinaryTree() {}
 
-	void setRoot(BinaryNode* node)	{ root = node; }
-	BinaryNode* getRoot()			{ return root; }
-	bool isEmpty()	{ return root==NULL; }
+	void setRoot(BinaryNode* node) { root = node; }
+	BinaryNode* getRoot() { return root; }
+	bool isEmpty() { return root == NULL; }
 
-	int	getCount()	{ return isEmpty() ? 0 : getCount(root); }
-	int	getLeafCount(){ return isEmpty() ? 0 : getLeafCount(root); }
-	int	getHeight()	{ return isEmpty() ? 0 : getHeight(root); }
-	void inorder()	{ printf("\n   inorder: "); inorder(root); }
-	void preorder()	{ printf("\n  preorder: "); preorder(root); }
-	void postorder(){ printf("\n postorder: "); postorder(root); }
-	void levelorder( ) {
+	int	getCount() { return isEmpty() ? 0 : getCount(root); }
+	int	getLeafCount() { return isEmpty() ? 0 : getLeafCount(root); }
+	int	getHeight() { return isEmpty() ? 0 : getHeight(root); }
+	void inorder() { printf("\n   inorder: "); inorder(root); }
+	void preorder() { printf("\n  preorder: "); preorder(root); }
+	void postorder() { printf("\n postorder: "); postorder(root); }
+	void levelorder() {
 		printf("\nlevelorder: ");
-		if( !isEmpty() ) {
+		if (!isEmpty()) {
 			CircularQueue q;
-			q.enqueue( root );
-			while ( !q.isEmpty() ) {
+			q.enqueue(root);
+			while (!q.isEmpty()) {
 				BinaryNode* n = q.dequeue();
-				if( n != NULL ) {
+				if (n != NULL) {
 					printf(" [%c] ", n->getData());
-					q.enqueue(n->getLeft ());
+					q.enqueue(n->getLeft());
 					q.enqueue(n->getRight());
 				}
 			}
@@ -37,68 +37,69 @@ public:
 		printf("\n");
 	}
 
-	int getCount(BinaryNode *node) {
-		if( node == NULL ) return 0;
+	int getCount(BinaryNode* node) {
+		if (node == NULL) return 0;
 		return 1 + getCount(node->getLeft()) + getCount(node->getRight());
 	}
-	int getLeafCount(BinaryNode *node) {
-		if( node == NULL ) return 0;
-		if( node->isLeaf() ) return 1;
+	int getLeafCount(BinaryNode* node) {
+		if (node == NULL) return 0;
+		if (node->isLeaf()) return 1;
 		else return getLeafCount(node->getLeft()) + getLeafCount(node->getRight());
 	}
-	int getHeight(BinaryNode *node)	{
-		if( node == NULL ) return 0;
-		int	hLeft  = getHeight(node->getLeft());
+	int getHeight(BinaryNode* node) {
+		if (node == NULL) return 0;
+		int	hLeft = getHeight(node->getLeft());
 		int	hRight = getHeight(node->getRight());
-		return (hLeft>hRight) ? hLeft+1 : hRight+1;
+		return (hLeft > hRight) ? hLeft + 1 : hRight + 1;
 	}
-	int	calcSize()	{ return calcSize(root); }
-	int calcSize(BinaryNode *node)	{
-		if( node == NULL ) return 0;
+	int	calcSize() { return calcSize(root); }
+	int calcSize(BinaryNode* node) {
+		if (node == NULL) return 0;
 		return node->getData() + calcSize(node->getLeft()) + calcSize(node->getRight());
 	}
 
 	int evaluate() { return evaluate(root); }
-	//           Լ 
-	int evaluate(BinaryNode *node) {
-		if( node == NULL ) return 0;
-	    if( node->isLeaf() ) return node->getData();
-	    else {
+	//
+	int evaluate(BinaryNode* node) {
+		if (node == NULL) return 0;
+		if (node->isLeaf()) return node->getData();
+		else {
 			int op1 = evaluate(node->getLeft());
 			int op2 = evaluate(node->getRight());
-			switch(node->getData()){
-				case '+': return op1+op2;
-				case '-': return op1-op2;
-				case '*': return op1*op2;
-				case '/': return op1/op2;
+			switch (node->getData()) {
+			case '+': return op1 + op2;
+			case '-': return op1 - op2;
+			case '*': return op1 * op2;
+			case '/': return op1 / op2;
 			}
 			return 0;
-	    }
+		}
 	}
 
 
 private:
-	// Ʈ       ȸ
-	void inorder(BinaryNode *node) {
-		if( node != NULL ) {
-			if( node->getLeft() != NULL ) inorder(node->getLeft());
-			printf( " [%c] ", node->getData());
-			if( node->getRight()!= NULL ) inorder(node->getRight());
+	//
+	void inorder(BinaryNode* node) {
+		if (node != NULL) {
+			if (node->getLeft() != NULL) inorder(node->getLeft());
+			printf(" [%c] ", node->getData());
+			if (node->getRight() != NULL) inorder(node->getRight());
 		}
 	}
-	void preorder(BinaryNode *node) {
-		if( node != NULL ) {
-			printf( " [%c] ", node->getData());
-			if( node->getLeft() != NULL ) preorder(node->getLeft());
-			if( node->getRight()!= NULL ) preorder(node->getRight());
+	void preorder(BinaryNode* node) {
+		if (node != NULL) {
+			printf(" [%c] ", node->getData());
+			if (node->getLeft() != NULL) preorder(node->getLeft());
+			if (node->getRight() != NULL) preorder(node->getRight());
 		}
 	}
-	void postorder(BinaryNode *node) {
-		if( node != NULL ) {
-			if( node->getLeft() != NULL ) postorder(node->getLeft());
-			if( node->getRight()!= NULL ) postorder(node->getRight());
-			printf( " [%c] ", node->getData());
+	void postorder(BinaryNode* node) {
+		if (node != NULL) {
+			if (node->getLeft() != NULL) postorder(node->getLeft());
+			if (node->getRight() != NULL) postorder(node->getRight());
+			printf(" [%c] ", node->getData());
 		}
 	}
 };
+
 
