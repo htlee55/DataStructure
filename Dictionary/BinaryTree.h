fd7ff2a7e31684f96169518cb74eb0503a8e6e43
htlee55 <htlee55@naver.com>
2026-05-27 17:04:30 +0900

    Add BinaryTree class implementation

---
+++ Dictionary/BinaryTree.h
@@ -0,0 +1,19 @@
+// BinaryTree.h: 이진트리 클래스
+#include "BinaryNode.h"
+
+class BinaryTree
+{
+protected:
+	BinaryNode* root;
+public:
+	BinaryTree() : root(NULL) {}
+	bool isEmpty() { return root == NULL; }
+	void inorder(BinaryNode* node) {
+		if (node != NULL) {
+			if (node->getLeft() != NULL) inorder(node->getLeft());
+			node->display();
+			if (node->getRight() != NULL) inorder(node->getRight());
+		}
+	}
+};
+
