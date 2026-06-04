f975dae6e1755a4cdab6c2ea98fb142404cb624e
htlee55 <htlee55@naver.com>
2026-06-04 13:48:42 +0900

    Add HeapNode class for heap node representation

---
+++ chapter10/HuffmanCode/HeapNode.h
@@ -0,0 +1,11 @@
+// HeapNode.h: 힙에 저장할 노드 클래스
+#include <cstdio>
+class HeapNode {
+	int		key;		// key 값
+public:
+	HeapNode(int key = 0) : key(key) { }
+	void setKey(int k) { key = k; }
+	int	 getKey() { return key; }
+	void display() { printf("\t%4d", key); }
+};
+
